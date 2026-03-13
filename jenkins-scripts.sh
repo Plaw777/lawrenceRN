884df3187573f4705f7fb31c52fe85823e499cc2
Plaw777 <good100ness@gmail.com>
2026-03-13 06:24:15 +0100

    jenkins-script lastest

--- jenkins-scripts.sh
+++ jenkins-scripts.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
-	    https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
- echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
-	     https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-	         /etc/apt/sources.list.d/jenkins.list > /dev/null
-  sudo apt-get update
-    sudo apt-get install fontconfig openjdk-17-jre
-      sudo apt-get install jenkins -y
-      sudo systemctl start jenkins
-      sudo systemctl enable jenkins
-      sudo systemctl status jenkins
+sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2026.key
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt update
+sudo apt install fontconfig openjdk-21-jre
+sudo apt install jenkins
+sudo systemctl start jenkins
+sudo systemctl enable jenkins
+sudo systemctl status jenkins
